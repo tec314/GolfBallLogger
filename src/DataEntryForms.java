7042abfaf342c4150f4bceb05351cceb8461a274
tec314 <tec314@comcast.net>
2024-08-02 22:54:10 -0400

    Adjusted GUI

--- src/DataEntryForms.java
+++ src/DataEntryForms.java
@@ -1,4 +1,5 @@
 import java.awt.EventQueue;
+import java.awt.*;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -23,6 +24,8 @@ import javax.swing.JComboBox;
 import javax.swing.JSpinner;
 import java.sql.*;
 import javax.swing.*;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.border.EtchedBorder;
 
 public class DataEntryForms {
 
@@ -111,74 +114,201 @@ public class DataEntryForms {
 		frame.getContentPane().add(panel);
 		panel.setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel_2 = new JPanel();
-		panel_2.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel.add(panel_2, BorderLayout.NORTH);
-		panel_2.setLayout(new GridLayout(0, 6, 10, 5));
+		JPanel InputPanel = new JPanel();
+		InputPanel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel.add(InputPanel, BorderLayout.NORTH);
 		
 		JLabel lblNewLabel = new JLabel("Ball Brand");
-		panel_2.add(lblNewLabel);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		JComboBox brandCombo = new JComboBox();
-		panel_2.add(brandCombo);
 		
 		newBrandText = new JTextField();
-		panel_2.add(newBrandText);
 		newBrandText.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel("Ball Type");
-		panel_2.add(lblNewLabel_1);
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		JComboBox typeCombo = new JComboBox();
-		panel_2.add(typeCombo);
 		
 		newTypeText = new JTextField();
-		panel_2.add(newTypeText);
 		newTypeText.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("Color");
-		panel_2.add(lblNewLabel_2);
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
 		
-		JComboBox colorCombo = new JComboBox();
-		panel_2.add(colorCombo);
+		JPanel ColorPanel = new JPanel();
+		ColorPanel.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JPanel panel_13 = new JPanel();
+		panel_13.setBackground(new Color(255, 255, 255));
+		ColorPanel.add(panel_13);
+		
+		JLabel lblNewLabel_6 = new JLabel("W");
+		panel_13.add(lblNewLabel_6);
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setBackground(new Color(255, 0, 0));
+		ColorPanel.add(panel_5);
+		
+		JLabel lblNewLabel_7 = new JLabel("R");
+		panel_5.add(lblNewLabel_7);
+		
+		JPanel panel_6 = new JPanel();
+		panel_6.setBackground(new Color(255, 128, 0));
+		ColorPanel.add(panel_6);
+		
+		JLabel lblNewLabel_8 = new JLabel("O");
+		panel_6.add(lblNewLabel_8);
+		
+		JPanel panel_7 = new JPanel();
+		panel_7.setBackground(new Color(255, 255, 0));
+		ColorPanel.add(panel_7);
+		
+		JLabel lblNewLabel_9 = new JLabel("Y");
+		panel_7.add(lblNewLabel_9);
+		
+		JPanel panel_8 = new JPanel();
+		panel_8.setBackground(new Color(198, 255, 0));
+		ColorPanel.add(panel_8);
+		
+		JLabel lblNewLabel_10 = new JLabel("L");
+		panel_8.add(lblNewLabel_10);
+		
+		JPanel panel_9 = new JPanel();
+		panel_9.setBackground(new Color(0, 255, 0));
+		ColorPanel.add(panel_9);
+		
+		JLabel lblNewLabel_11 = new JLabel("G");
+		panel_9.add(lblNewLabel_11);
+		
+		JPanel panel_10 = new JPanel();
+		panel_10.setBackground(new Color(0, 128, 0));
+		ColorPanel.add(panel_10);
+		
+		JLabel lblNewLabel_12 = new JLabel("F");
+		panel_10.add(lblNewLabel_12);
+		
+		JPanel panel_11 = new JPanel();
+		panel_11.setBackground(new Color(0, 206, 230));
+		ColorPanel.add(panel_11);
+		
+		JLabel lblNewLabel_13 = new JLabel("C");
+		panel_11.add(lblNewLabel_13);
+		
+		JPanel panel_12 = new JPanel();
+		panel_12.setBackground(new Color(0, 128, 255));
+		ColorPanel.add(panel_12);
+		
+		JLabel lblNewLabel_14 = new JLabel("B");
+		panel_12.add(lblNewLabel_14);
+		
+		JPanel panel_14 = new JPanel();
+		panel_14.setBackground(new Color(255, 0, 255));
+		ColorPanel.add(panel_14);
+		
+		JLabel lblNewLabel_15 = new JLabel("P");
+		panel_14.add(lblNewLabel_15);
+		
+		JPanel panel_15 = new JPanel();
+		panel_15.setBackground(new Color(128, 0, 255));
+		ColorPanel.add(panel_15);
+		
+		JLabel lblNewLabel_16 = new JLabel("V");
+		panel_15.add(lblNewLabel_16);
 		
 		newColorText = new JTextField();
-		panel_2.add(newColorText);
 		newColorText.setColumns(10);
 		
 		JLabel lblNewLabel_4 = new JLabel("Pattern");
-		panel_2.add(lblNewLabel_4);
+		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		JComboBox patternCombo = new JComboBox();
-		panel_2.add(patternCombo);
 		
 		newPatternText = new JTextField();
-		panel_2.add(newPatternText);
 		newPatternText.setColumns(10);
 		
 		JLabel lblNewLabel_3 = new JLabel("Logo");
-		panel_2.add(lblNewLabel_3);
+		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		JComboBox logoCombo = new JComboBox();
-		panel_2.add(logoCombo);
 		
 		newLogoText = new JTextField();
-		panel_2.add(newLogoText);
 		newLogoText.setColumns(10);
 		
 		JLabel lblNewLabel_5 = new JLabel("Ball Number");
-		panel_2.add(lblNewLabel_5);
+		lblNewLabel_5.setHorizontalAlignment(SwingConstants.CENTER);
+		InputPanel.setLayout(new GridLayout(0, 6, 20, 5));
+		InputPanel.add(lblNewLabel);
+		InputPanel.add(brandCombo);
+		InputPanel.add(newBrandText);
+		InputPanel.add(lblNewLabel_1);
+		InputPanel.add(typeCombo);
+		InputPanel.add(newTypeText);
+		InputPanel.add(lblNewLabel_2);
+		InputPanel.add(ColorPanel);
+		InputPanel.add(newColorText);
+		InputPanel.add(lblNewLabel_4);
+		InputPanel.add(patternCombo);
+		InputPanel.add(newPatternText);
+		InputPanel.add(lblNewLabel_3);
+		InputPanel.add(logoCombo);
+		InputPanel.add(newLogoText);
+		InputPanel.add(lblNewLabel_5);
 		
-		JSpinner ballNumberSpinner = new JSpinner();
-		panel_2.add(ballNumberSpinner);
+		JPanel BallNumberPanel = new JPanel();
+		InputPanel.add(BallNumberPanel);
+		BallNumberPanel.setLayout(new GridLayout(1, 0, 10, 0));
+		
+		JPanel panel_16 = new JPanel();
+		panel_16.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		panel_16.setBackground(new Color(255, 255, 255));
+		BallNumberPanel.add(panel_16);
+		
+		JLabel lblNewLabel_17 = new JLabel("1");
+		panel_16.add(lblNewLabel_17);
+		
+		JPanel panel_17 = new JPanel();
+		panel_17.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		panel_17.setBackground(new Color(255, 255, 255));
+		BallNumberPanel.add(panel_17);
+		
+		JLabel lblNewLabel_18 = new JLabel("2");
+		panel_17.add(lblNewLabel_18);
+		
+		JPanel panel_18 = new JPanel();
+		panel_18.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		panel_18.setBackground(new Color(255, 255, 255));
+		BallNumberPanel.add(panel_18);
+		
+		JLabel lblNewLabel_19 = new JLabel("3");
+		panel_18.add(lblNewLabel_19);
+		
+		JPanel panel_19 = new JPanel();
+		panel_19.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		panel_19.setBackground(new Color(255, 255, 255));
+		BallNumberPanel.add(panel_19);
+		
+		JLabel lblNewLabel_20 = new JLabel("4");
+		panel_19.add(lblNewLabel_20);
 		
 		JPanel panel_3 = new JPanel();
-		panel_3.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel.add(panel_3, BorderLayout.CENTER);
-		panel_3.setLayout(new BorderLayout(0, 0));
+		InputPanel.add(panel_3);
+		panel_3.setLayout(new GridLayout(1, 0, 10, 0));
+		
+		JSpinner ballNumberSpinner = new JSpinner();
+		panel_3.add(ballNumberSpinner);
+		
+		JPanel panel_20 = new JPanel();
+		panel_3.add(panel_20);
+		
+		JPanel DatabasePanel = new JPanel();
+		DatabasePanel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel.add(DatabasePanel, BorderLayout.CENTER);
+		DatabasePanel.setLayout(new BorderLayout(0, 0));
 		
 		JScrollPane scrollPane = new JScrollPane();
-		panel_3.add(scrollPane);
+		DatabasePanel.add(scrollPane);
 		
 		table = new JTable();
 		table.setFillsViewportHeight(true);
@@ -188,10 +318,15 @@ public class DataEntryForms {
 			new String[] {
 			}
 		));
+		table.setDefaultEditor(Object.class, null); // Makes table not editable
 		scrollPane.setViewportView(table);
 		
 		JPanel panel_1 = new JPanel();
-		panel.add(panel_1, BorderLayout.SOUTH);
+		panel_1.setBackground(new Color(255, 255, 255));
+		DatabasePanel.add(panel_1, BorderLayout.NORTH);
+		
+		JPanel ActionPanel = new JPanel();
+		panel.add(ActionPanel, BorderLayout.SOUTH);
 		
 		JButton btnNewButton = new JButton("Add Entry");
 		btnNewButton.addActionListener(new ActionListener() {
@@ -216,16 +351,16 @@ public class DataEntryForms {
 				}
 			}
 		});
-		panel_1.add(btnNewButton);
+		ActionPanel.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Update");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		panel_1.add(btnNewButton_1);
+		ActionPanel.add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("Delete");
-		panel_1.add(btnNewButton_2);
+		ActionPanel.add(btnNewButton_2);
 	}
 }
